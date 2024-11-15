946b54844568d8947a1fffd86359246c310313a9
Romain Brault <mail@romainbrault.com>
2024-11-15 06:14:01 +0000

    feat: ✨ add command line interface

---
+++ docs/usage.md
@@ -0,0 +1,22 @@
+<!--
+SPDX-FileCopyrightText: © 2024 Romain Brault <mail@romainbrault.com>
+
+SPDX-License-Identifier: CC0-1.0
+-->
+
+# 💻 Usage
+
+```{eval-rst}
+
+.. click:: test_rtd_linux_py313_test.cli.entrypoint:test_rtd_linux_py313_test
+    :prog: whiteprint
+    :nested: full
+```
+
+```{toctree}
+---
+hidden:
+---
+
+Usage <self>
+```
--- src/test_rtd_linux_py313_test/__init__.py
+++ src/test_rtd_linux_py313_test/__init__.py
@@ -4,10 +4,22 @@
 
 """Top-level module."""
 
+import importlib
 from typing import Final
 
+from test_rtd_linux_py313_test.environment import ENVIRONMENT_FILE, load_dotenv
 from test_rtd_linux_py313_test.package_metadata import __version__
 
 
 __all__: Final = ["__version__"]
 """Public module attributes."""
+
+
+if __debug__:
+    beartype = importlib.import_module("beartype")
+    beartype_claw = importlib.import_module("beartype.claw")
+    beartype_claw.beartype_this_package(
+        conf=beartype.BeartypeConf(is_color=False),
+    )
+
+load_dotenv(ENVIRONMENT_FILE)
